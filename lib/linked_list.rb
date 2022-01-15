47743279e8d6d5d6b3cede063cf263a74792f771
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-14 18:57:16 -0600

    commit tree 16112aefaf52d424ed0fdf1ac08abfa2bef6d4fb

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,18 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    current_node = tail
+    until current_node == head do
+      next_node = current_node.prev_node
+      current_node.prev_node = current_node.next_node
+      current_node.next_node = next_node
+      current_node = next_node
+    end
+    self.head = tail
+    self.tail = current_node
+    if tail
+      tail.prev_node = tail.next_node
+      tail.next_node = nil
+    end
   end
 end
